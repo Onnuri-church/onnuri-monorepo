253e98bdaaa7c5b332490a0a556c61613a9ca55d
s576air <s576ai@gmail.com>
2026-07-28 21:32:16 +0900

    SCRUM-63 refactor: 설정 import를 상대경로로 변경

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
+import { type TypedConfigService } from '../../config/configuration';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { type TypedConfigService } from 'src/config/configuration';
 
 @Module({
   imports: [
--- apps/api/src/modules/auth/strategies/jwt.strategy.ts
+++ apps/api/src/modules/auth/strategies/jwt.strategy.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { type AppConfig } from 'src/config/configuration';
+
+import { type AppConfig } from '../../../config/configuration';
 
 export interface JwtPayload {
   sub: string;
